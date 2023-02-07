5d77ff52e22906171a7b062d571410ae467564e4
Peter van Meijgaard <petervanmeijgaard@gmail.com>
2023-02-07 11:21:03 +0100

    :heavy_minus_sign: Remove font awesome from project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import 'destyle.css';
-import '@fortawesome/fontawesome-free/css/all.css';
-import '@fortawesome/fontawesome-free/js/all';
 
 import './assets/styles/app.scss';
 
